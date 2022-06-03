b0762c29951e9ba9f651cd67a675b38f03e5e841
Abiprodigy <abateb2021@gmail.com>
2022-06-03 16:50:56 -0400

    js rectified

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,5 @@
 var playerName = window.prompt ("What is your robot's name?");
 console.log(playerName);
-console.log ("Our robot's name is " + playerName);
-console.log ("Sanjaw " + playerName);
-var name = "ethiopia";
-console.log ("Andegna " + name);
-
-var x = 20;
-x*=40;
-console.log (x);
-console.log ("Buffon cafu carlos cr7");
-
-
 function fight() {
  window.alert("The fight has begun!");
 }
